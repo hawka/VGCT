59f02081936a101097c171b3d1341882713878ca
Amalia Hawkins <amopet4@gmail.com>
2012-01-22 18:27:01 -0500

    commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VGCT::Application.config.secret_token = '1804074a9b469fabc7ca2f0e9cd0ac685bee9bac04fcdd64d6a98c083212cc7deccee25380b2cdc3f204aeeb2f7fda15e11aa16a13f30a8d880dedb1cf1af940'
