1269cf7bf1b0db1a4e29e8d80cb31841f7128126
Amalia Hawkins <amopet4@gmail.com>
2012-02-13 11:08:56 -0500

    starting from scratch.......

---
+++ VGCT/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VGCT::Application.config.secret_token = 'd596aaa3eb0e55e74393b7665e6c8206e6fec660776c439be219d27fde85608dc097e68f46afe58dd9f6f560f06f6cc6d639d8ad2f8b21b638abc8a3a54b0fae'
