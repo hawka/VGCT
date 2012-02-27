d179bd04dfae6a48fdce454e8cde25046249818d
Amalia Hawkins <amopet4@gmail.com>
2012-02-27 03:43:05 -0500

    fixed bug with player truename validation

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,5 +1,5 @@
 class Player < ActiveRecord::Base
   validates :username, presence: true
   validates :username, uniqueness: true
-  validates :truename, :format => { :with => /\A[a-zA-Z]+\z/, :message => "truename may only use letters" }
+  validates :truename, :format => { :with => /\A(\w+\s*)+\z/, :message => "truename is improperly formatted" }
 end
