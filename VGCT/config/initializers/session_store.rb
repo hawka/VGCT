87a4db6b0f79c43d1a26095440b19b3ef7438bc5
Amalia Hawkins <amopet4@gmail.com>
2012-05-04 12:42:13 -0400

    removed files

--- VGCT/config/initializers/session_store.rb
+++
@@ -1,8 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-VGCT::Application.config.session_store :cookie_store, key: '_VGCT_session'
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rails generate session_migration")
-# VGCT::Application.config.session_store :active_record_store
